cd130599e1b7e5104e64a4d9cfcc5df752f636c8
zessirb <zessirb@users.noreply.github.com>
2020-12-26 10:29:21 +0100

    Fixed source_me shebang

--- source_me.sh
+++ source_me.sh
@@ -1,4 +1,4 @@
-#/bin/sh 
+#!/bin/sh 
 
 export AWS_ACCESS_KEY_ID=`grep aws_access_key_id ~/.aws/credentials | awk '{print $3}'`
 export AWS_SECRET_ACCESS_KEY=`grep aws_secret_access_key ~/.aws/credentials | awk '{print $3}'`
